2edef9d59fa7244b1c907c6c83d10595f03042c4
Sean Francis <polaris@vmi1316688.contaboserver.net>
2023-09-24 18:14:21 -0700

    update readme.md and setting up env variable

--- README.md
+++ README.md
@@ -5,6 +5,12 @@ Install Dependencies:
 
 ### `yarn`
 
+Setting Up the Environment:
+
+You should replace the `REACT_APP_IMDB_API_KEY` to the key I provide to you via email
+
+### `cp .env.example .env`
+
 Build project for local:
 
 ### `yarn start`
--- src/constants/config.js
+++ src/constants/config.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://imdb-api.com/en/API";
-export const API_KEY = "k_yz8f6095";
+export const API_URL = process.env.REACT_APP_IMDB_URL || "https://imdb-api.com/en/API"
+export const API_KEY = process.env.REACT_APP_IMDB_API_KEY || "test_key"
